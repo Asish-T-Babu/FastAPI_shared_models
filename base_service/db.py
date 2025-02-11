65fb690fab5a175d81de0f1bb510106679089d62
asish-t-babu <asishtbabu@gmail.com>
2025-02-12 00:52:50 +0530

    Addeded DB session

--- base_service/db.py
+++ base_service/db.py
@@ -6,3 +6,10 @@ DATABASE_URL = "postgresql://postgres:password@localhost/two_in_one_repo"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
